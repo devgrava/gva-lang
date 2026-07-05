05c983f41acbfd9ff33a4c8540d30dcf4fded01d
devgrava <devgrava@gmail.com>
2026-07-06 04:15:24 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# gva-lang
+# GVA-Lang
+
+GVA Programming Language
+
+## Version
+
+0.1.0
+
+## Run
+
+```bash
+npm start
+```
+
+## Author
+
+Didit Farafat
---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("===================================")
+console.log("      GVA Programming Language")
+console.log("            Version 0.1.0")
+console.log("===================================")
